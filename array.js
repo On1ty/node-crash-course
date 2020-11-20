00d6c34ca57c565a8ad1fa84d50dcebe3c1aceb6
On1ty <juanity.andoy@gmail.com>
2020-11-20 16:11:35 +0800

    Node Basic Practice

---
+++ array.js
@@ -0,0 +1,12 @@
+/**
+ * Data to be exported
+ */
+const arr = ['Noob', 'Dev', 'On1ty'];
+const ages = ['21', 'TwentyOne', '2ne1'];
+
+// console.log(arr);
+
+module.exports = {
+    arr,
+    ages,
+};
--- index.js
+++ index.js
@@ -1,26 +1,97 @@
-//Node Basic
-const name = "NoobDev";
+/**
+ * Node Basic
+ */
+// const name = "NoobDev";
 
-console.log(name);
+// console.log(name);
 
-const greet = (name) => {
-    console.log(`hello, ${name}`)
-}
+// const greet = (name) => {
+//     console.log(`hello, ${name}`)
+// }
 
-greet('Noob');
-greet('Dev');
+// greet('Noob');
+// greet('Dev');
 
-//Global Object
-console.log(global)
+/**
+ * Global Object
+ */
+// console.log(global)
 
-setTimeout(() => {
-    console.log('NoobDev');
-    clearInterval(int)
-}, 3000);
+// setTimeout(() => {
+//     console.log('NoobDev');
+//     clearInterval(int)
+// }, 3000);
 
-const int = setInterval(() => {
-    console.log('NoobDev');
-}, 1000);
+// const int = setInterval(() => {
+//     console.log('NoobDev');
+// }, 1000);
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+
+/**
+ * Modules
+ */
+//import data from ./array.js
+// const {
+//     arr,
+//     ages
+// } = require('./array');
+
+// console.log(arr, ages);
+
+// const os = require('os');
+// console.log(os.platform(), os.homedir());
+
+/**
+ * File System
+ */
+
+const fs = require('fs');
+
+//reading files
+// fs.readFile('./blog.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+// console.log('Last Line');
+
+//writing files
+// fs.writeFile('./blog.txt', 'Hello NoobDev', () => {
+//     console.log('File was written')
+// });
+
+//directories
+// if (!fs.existsSync('./assets')) {
+//     fs.mkdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         console.log('Folder created!')
+//     });
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+
+//         console.log('Folder Deleted')
+//     })
+// }
+
+//deleting files
+// if (fs.existsSync('./blog.txt')) {
+//     fs.unlink('./blog.txt', (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+
+//         console.log('File Deleted')
+//     })
+// }
+
+/**
+ * Streams & Buffers
+ */
