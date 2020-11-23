026a52229f9e033bf8ff696724d5384cd689188c
On1ty <juanity.andoy@gmail.com>
2020-11-23 13:13:52 +0800

    Express JS

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+//express app
+const app = express();
+
+//listen for request
+const port = 3000;
+app.listen(port);
+
+app.get('/', (req, res) => {
+
+    //res.send('<p>Home page</p>');
+    res.sendFile('./views/index.html', {
+        root: __dirname
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.sendFile('./views/about.html', {
+        root: __dirname
+    });
+});
+
+//redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+//404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {
+        root: __dirname
+    });
+})
